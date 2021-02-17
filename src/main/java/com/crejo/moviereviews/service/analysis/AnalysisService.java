513f855a2b428b3c6a043937db9b63322d7f6920
Rushil Chugh <rushilchugh01@gmail.com>
2021-02-17 19:49:19 +0530

    Implemented everything apart from review limit

--- src/main/java/com/crejo/moviereviews/service/analysis/AnalysisService.java
+++ src/main/java/com/crejo/moviereviews/service/analysis/AnalysisService.java
@@ -1,11 +1,12 @@
 package com.crejo.moviereviews.service.analysis;
 
 import com.crejo.moviereviews.model.Movie;
+import com.crejo.moviereviews.service.movie.MovieGenre;
 
 import java.util.List;
 
 public interface AnalysisService {
-    List<Movie> topNMovies(Integer n);
+    List<Movie> topNMovies(MovieGenre movieGenre, Integer n);
     Double averageReviewScoreByYear(Integer year);
     Double averageReviewScoreByMovie(String movieTitle);
 }
---
+++ src/main/java/com/crejo/moviereviews/service/analysis/AnalysisServiceImpl.java
@@ -0,0 +1,49 @@
+package com.crejo.moviereviews.service.analysis;
+
+import com.crejo.moviereviews.model.Movie;
+import com.crejo.moviereviews.model.Review;
+import com.crejo.moviereviews.service.movie.MovieGenre;
+import com.crejo.moviereviews.service.movie.MovieRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AnalysisServiceImpl implements AnalysisService {
+
+    private MovieRepository movieRepository;
+
+    public AnalysisServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public List<Movie> topNMovies(MovieGenre movieGenre, Integer n) {
+        return movieRepository.listMovies().stream().filter(movie -> movie.getMovieGenres().contains(movieGenre))
+                .sorted(Comparator.comparingInt(this::sumOfReviews))
+                .limit(n)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public Double averageReviewScoreByYear(Integer year) {
+        return movieRepository.listMovies().stream().filter(movie -> movie.getReleaseYear().equals(year))
+                .map(this::sumOfReviews)
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+    }
+
+    @Override
+    public Double averageReviewScoreByMovie(String movieTitle) {
+        return movieRepository.listMovies().stream().filter(movie -> movie.getMovieTitle().equals(movieTitle))
+                .map(this::sumOfReviews)
+                .mapToDouble(i -> i)
+                .average().getAsDouble();
+    }
+
+    private Integer sumOfReviews(Movie movie) {
+        return movie.getReviews().stream().map(Review::getScore).mapToInt(Double::intValue).sum();
+    }
+}
