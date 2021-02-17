010e69e22a0bc16cba20ba7c7a6cf1931c0c580d
Rushil Chugh <rushilchugh01@gmail.com>
2021-02-17 16:05:09 +0530

    Initial commit

---
+++ src/main/java/com/crejo/moviereviews/MoviereviewsApplication.java
@@ -0,0 +1,13 @@
+package com.crejo.moviereviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crejo/moviereviews/MoviereviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crejo.moviereviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
