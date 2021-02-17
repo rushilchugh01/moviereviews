bd15d1117ffc3f2da3ce67a350f0d53df39682d1
Rushil Chugh <rushilchugh01@gmail.com>
2021-02-17 19:55:16 +0530

    Review addition observer to a springbean and critical number from properties file

--- src/main/java/com/crejo/moviereviews/service/review/ReviewAdditionObserverImpl.java
+++ src/main/java/com/crejo/moviereviews/service/review/ReviewAdditionObserverImpl.java
@@ -3,22 +3,28 @@ package com.crejo.moviereviews.service.review;
 import com.crejo.moviereviews.model.User;
 import com.crejo.moviereviews.service.user.UserType;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class ReviewAdditionObserverImpl implements ReviewAdditionObserver {
 
     private Map<User, Integer> userReviewsMap;
+    private Integer criticalNumberVal;
 
-    @Value("${user.reviews.number.critic:3}")
-    private Integer criticNumberValue;
+    public ReviewAdditionObserverImpl(
+            @Value("${user.reviews.number.critic:3}") Integer criticalNumberVal) {
+        this.criticalNumberVal = criticalNumberVal;
+    }
+
 
     @Override
     public void notify(User user) {
         userReviewsMap.putIfAbsent(user, 1);
         userReviewsMap.computeIfPresent(user, (k, v) -> v+1);
 
-        if (userReviewsMap.get(user) >= criticNumberValue &&
+        if (userReviewsMap.get(user) >= criticalNumberVal &&
                 !user.getUserType().equals(UserType.CRITIC)) {
             user.setUserType(UserType.CRITIC);
         }
